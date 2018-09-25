31180103b9c92a282acd08de0bb73bac0e7a8a86
Viet Vo <viet.allen.qvo@gmail.com>
2018-09-24 20:25:59 -0700

    friend initial array

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,11 @@
+var friendArray = [
+    {
+      name: "my name Jeff",
+      phoneNumber: "000-000-0000",
+      picUrl: "https://via.placeholder.com/350x150"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
+  
