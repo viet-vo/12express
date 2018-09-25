6e48f0230a08105108ea51176d0d0b8fcbbf1083
Viet Vo <viet.allen.qvo@gmail.com>
2018-09-24 21:26:54 -0700

    change port to 3000

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
